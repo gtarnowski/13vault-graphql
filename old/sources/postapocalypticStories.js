e2640e8759cf05e57ef40ffddbacd940882053f7
gtarnowski <tarnasm89@gmail.com>
2018-06-23 18:38:50 +0200

    wip

---
+++ old/sources/postapocalypticStories.js
@@ -0,0 +1,21 @@
+export default {
+  mainSeparatorRegEx: /<div[^<>]*class="separator2"[^<>]*>[\\s\\S]*?<\/div>/gm,
+  imageSeparatorClass: 'imgksiazki',
+  fieldSplitterClass: 'niebieski',
+  htmlContent: `<div class="separator2" id="WroPit">Piter</div>
+<p><img src="obrazki/postkultura/ksiazki/piter.jpg" alt="Okładka książki 'Piter'" title="Szymun Wroczek - Piter" class="imgksiazki">
+ <span class="niebieski">Autor:</span> Szymun Wroczek<br>
+ <span class="niebieski">Tytuł:</span> Piter<br>
+ <span class="niebieski">Wydawnictwo:</span> Insignis<br>
+ <span class="niebieski">Tłumaczenie:</span> Paweł Podmiotko<br>
+ <span class="niebieski">Data wydania:</span> 2011<br>
+ <span class="niebieski">Liczba stron:</span> 600<br>
+ <span class="niebieski">ISBN:</span> 978-83-61428-46-6 </p>
+ <p>Zapoczątkowany w 2009 roku przez Dmitrija Głuchowskiego, projekt "Uniwersum Metro 2033" to seria książek, będących kontynuacją idei zawartej w popularnej powieści science-fiction "Metro 2033". Ta na dobrą sprawę kanoniczna już lektura w "klimatach" postapokalipsy stała się inspiracją dla równo dwudziestu już wydanych pod szyldem "Uniwersum Metro 2033" powieści <span class="nawias">(oraz zbioru opowiadań autorstwa czytelników rosyjskiej strony internetowej projektu)</span>, a także trzech utworów, które zostaną wydane w Rosji w pierwszym kwartale bieżącego roku <span class="nawias">(dane z początku stycznia 2012)</span>. Autorzy zdołali dotąd opisać szereg lokalizacji, sukcesywnie rozbudowując poatomowy świat, wykreowany przez Głuchowskiego - poza Moskwą m.in. Kijów, Krasnodar, Soczi, gros rosyjskich obwodów, a nawet Wielką Brytanię, Antarktydę i Afrykę <span class="nawias">(w planach są książki rozgrywające się m.in. w Charkowie, Kazaniu, Mińsku i Włoszech)</span>. Książka <span class="p-name2">"Piter"</span>, rosyjskiego pisarza ukrywającego się pod pseudonimem Szymun Wroczek, jest chronologicznie trzecią częścią "Uniwersum Metro 2033" <span class="nawias">(nie licząc książek Głuchowskiego)</span>, natomiast pierwszą wydaną w Polsce.</p>
+ <p align="center"><a href="ie_ksiazki_piter.htm"><span class="minus2"><b>[Czytaj całość recenzji]</b></span></a></p>
+ <p align="right">© 2012 <strong>Veron</strong></p>
+ <table width="100%"><tbody><tr>
+ <td align="right"><a href="#menu"><strong>⇒ Menu</strong></a></td>
+ </tr></tbody></table>
+<br>`
+}
